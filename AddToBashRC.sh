2c23c857a33d5471fda93c971ef3133242b18774
alanmun <aem108@pitt.edu>
2024-03-22 00:11:11 -0400

    More script ideas

--- AddToBashRC.sh
+++ AddToBashRC.sh
@@ -1,6 +1,15 @@
 # The below snippets are meant to be pasted into a bashrc.
 # source ~/.bashrc to reload
 
+# Use neovim if present. Fallback to vim. Fallback to vi
+if command -v nvim >/dev/null 2>&1; then
+    export EDITOR='nvim'
+elif command -v vim >/dev/null 2>&1; then
+    export EDITOR='vim'
+else
+    export EDITOR='vi'
+fi
+
 # c - a function to go to a directory and "see" its contents
 c() {
     cd "$1" && ls -a
---
+++ DebianDevEnv.sh
@@ -0,0 +1,7 @@
+# This script is for one time running of anything that I'd want in a development environment for Debian based distros like Ubuntu.
+
+sudo apt update
+sudo apt install vim tree
+
+# Idea: Would be cool to have something in here that injects AddToBashRC.sh into my bash rc file. Then do
+# source ~/.bashrc
